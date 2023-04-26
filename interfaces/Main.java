e4dee3f679880317d7897ec816b409c448fac031
NewB-Typer <67378746+NewB-Typer@users.noreply.github.com>
2023-04-26 15:17:03 +0000

    Add files via upload

---
+++ interfaces/Main.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public class Main {
+	public static void main(String[] args) {
+player Player = new player();
+Player.apps();
+Player.caps();
+
+	}
+	}
---
+++ interfaces/player.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public class player implements Club,Nation{
+void apps() {
+	System.out.println("Player has -- appearance for club.");
+}
+	
+	void caps() {
+		System.out.println("Player has -- no. of caps for nation");
+	}
+}
